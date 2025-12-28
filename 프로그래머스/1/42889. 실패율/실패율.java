77c99d015e19eb712bb9591fe03bab9429fffc35
jinseo4829 <chjs9284@naver.com>
2025-12-29 02:47:51 +0900

    [level 1] Title: 실패율, Time: 10.42 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        
+        //스테이지별 도전자 수 구하기
+        int[] challenger = new int[N+2]; 
+        for(int i=0; i<stages.length; i++) {
+            challenger[stages[i]] += 1;
+        }
+        
+        //실패율 구하기
+        HashMap<Integer, Double> fails = new HashMap<>();
+        double total = stages.length;
+        
+        for(int i=1; i<= N; i++) {
+            if(challenger[i] == 0) {
+                fails.put(i, 0.);
+            }
+            else{
+                fails.put(i, challenger[i] / total);
+                total -= challenger[i];
+            }
+        }
+        
+        return fails.entrySet().stream().sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue()))
+               .mapToInt(HashMap.Entry::getKey).toArray();
+    }
+}
