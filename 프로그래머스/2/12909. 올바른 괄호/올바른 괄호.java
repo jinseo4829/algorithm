31bf16c2488b9ebe0f01850f345062909a89e337
jinseo4829 <chjs9284@naver.com>
2026-01-03 01:49:06 +0900

    [level 2] Title: 올바른 괄호, Time: 20.46 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.Stack; 
+
+class Solution {
+    boolean solution(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        char[] a = s.toCharArray();
+        
+        for(char c : a) {
+            if(c == '(') {
+                stack.push(c);
+            }else{
+                if(stack.isEmpty() || stack.pop() == c) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
