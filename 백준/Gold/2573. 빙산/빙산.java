8f29951b90599a7ea2ce39a52bdbf3007064f3a3
jinseo4829 <chjs9284@naver.com>
2026-03-24 21:09:41 +0900

    [Gold IV] Title: 빙산, Time: 520 ms, Memory: 134476 KB -BaekjoonHub

---
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -0,0 +1,119 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int year = 0;
+
+        while (true) {
+            int count = countIceberg();
+
+            if (count >= 2) {
+                System.out.println(year);
+                return;
+            }
+
+            if (count == 0) {
+                System.out.println(0);
+                return;
+            }
+
+            melt();
+            year++;
+        }
+    }
+
+    public static int countIceberg() {
+        visited = new boolean[N][M];
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] > 0 && !visited[i][j]) {
+                    dfs(i, j);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+                continue;
+            }
+
+            if (map[nx][ny] > 0 && !visited[nx][ny]) {
+                dfs(nx, ny);
+            }
+        }
+    }
+
+    public static void melt() {
+        int[][] meltAmount = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] > 0) {
+                    int seaCount = 0;
+
+                    for (int d = 0; d < 4; d++) {
+                        int nx = i + dx[d];
+                        int ny = j + dy[d];
+
+                        if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+                            continue;
+                        }
+
+                        if (map[nx][ny] == 0) {
+                            seaCount++;
+                        }
+                    }
+
+                    meltAmount[i][j] = seaCount;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] > 0) {
+                    map[i][j] -= meltAmount[i][j];
+                    if (map[i][j] < 0) {
+                        map[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
